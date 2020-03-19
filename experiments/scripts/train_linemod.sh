b01f67b86af08305bc81cb8124e3afcab7886281
tevon <twalker81@gatech.edu>
2020-03-18 20:38:42 -0400

    Add ignoring of warnings

--- experiments/scripts/train_linemod.sh
+++ experiments/scripts/train_linemod.sh
@@ -6,5 +6,5 @@ set -e
 export PYTHONUNBUFFERED="True"
 export CUDA_VISIBLE_DEVICES=0
 
-python3 ./tools/train.py --dataset linemod\
+python3 -W ignore ./tools/train.py --dataset linemod\
   --dataset_root ./datasets/linemod/Linemod_preprocessed
